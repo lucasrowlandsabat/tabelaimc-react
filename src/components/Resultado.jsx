6c45e7198caec43356629513c80c3213b6715ce5
Lucas <abatrlucas@gmail.com>
2025-08-22 10:00:17 -0300

    Criando a pasta TabelaImc separada

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,44 +1,8 @@
 import '../css/global.css'
 import '../css/resultado.css'
+import TabelaImc from './TabelaImc'
 
-const TabelaImc =(resultado)=>{
-    if(resultado < 18){
-        return(
-            <>
-            <td>Abaixo do peso</td>
-            <td>Abaixo de 18</td>
-            </>
-        )
-    }else if(resultado >18 && resultado <25){
-        return(
-            <>
-            <td>Peso normal</td>
-            <td>18 - 25.9</td>
-            </>
-        );
-    }else if(resultado > 24 && resultado <30){
-        return(
-            <>
-            <td>Sobre peso</td>
-            <td>25 - 29.9</td>
-            </>
-        );
-    }else if(resultado> 29 && resultado <35){
-        return(
-            <>
-            <td>Obesidade Grau-I</td>
-            <td>30-34.9</td>
-            </>
-        );
-    }else{
-        return(
-            <>
-            <td>Obesidade Mórbida</td>
-            <td>Maior ou igual a 40</td>
-            </>
-        )
-    }
-}
+
 
 //destruct - desestruturação = acessa propiedades de outro componente
 function Resultado({resultado}){
